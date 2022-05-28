1a4956e9967d7b164a5207e7d3d3347f69996b70
CuriouslyCurious <thecuriouslycurious@protonmail.com>
2022-05-28 14:55:32 +0200

    Replace fzf with sk

--- functions/_fzf_wrapper.fish
+++ functions/_fzf_wrapper.fish
@@ -4,17 +4,17 @@ function _fzf_wrapper --description "Prepares some environment variables before 
     # Use --local so that it doesn't clobber SHELL outside of this function.
     set --local --export SHELL (command --search fish)
 
-    # If FZF_DEFAULT_OPTS is not set, then set some sane defaults.
+    # If SKIM_DEFAULT_COMMAND is not set, then set some sane defaults.
     # See https://github.com/junegunn/fzf#environment-variables
-    if not set --query FZF_DEFAULT_OPTS
+    if not set --query SKIM_DEFAULT_COMMAND
         # cycle allows jumping between the first and last results, making scrolling faster
         # layout=reverse lists results top to bottom, mimicking the familiar layouts of git log, history, and env
         # border shows where the fzf window begins and ends
         # height=90% leaves space to see the current command and some scrollback, maintaining context of work
         # preview-window=wrap wraps long lines in the preview window, making reading easier
         # marker=* makes the multi-select marker more distinguishable from the pointer (since both default to >)
-        set --export FZF_DEFAULT_OPTS '--cycle --layout=reverse --border --height=90% --preview-window=wrap --marker="*"'
+        set --export SKIM_DEFAULT_COMMAND '--cycle --layout=reverse --border --height=90% --preview-window=wrap --marker="*"'
     end
 
-    fzf $argv
+    sk $argv
 end
